5613bb9926fc2bc9826e7d73609b5308c341603c
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-01-11 01:21:36 +0800

    Update c10.py

--- c10.py
+++ c10.py
@@ -1,12 +1,9 @@
-#this is my new code
+#this is my new c9
 n=int(input())
-if n%2==0:
-    while n>1:
-        n=n/2
-    if n==1:
-        print("yes")
-    else:
-        print("no")
+while n>1:
+    n=n/2
+if n==1:
+    print("yes")
 else:
     print("no")
     
